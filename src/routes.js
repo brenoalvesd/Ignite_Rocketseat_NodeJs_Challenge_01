164eb599849f31b66708ebe58fbef162cbebd9a0
Breno Alves <breno7572@gmail.com>
2024-05-27 18:00:01 -0300

    Criando enpoint DELETE

--- src/routes.js
+++ src/routes.js
@@ -78,5 +78,25 @@ export const routes = [
 
       return res.writeHead(204).end('Task updated successfully!')
     }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+    
+      if (!id) {
+        return res.writeHead(404).end('ID is required');
+      }
+
+      const [task] = database.select('tasks', { id })
+
+      if (!task) {
+        return res.writeHead(404).end('There is no task with this given ID')
+      } else {
+        database.delete('tasks', id)
+        return res.writeHead(204).end('Task deleted successfully!')
+      }
+    }
   }
 ]
