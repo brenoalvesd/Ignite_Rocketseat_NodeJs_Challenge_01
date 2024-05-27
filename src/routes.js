826ca666e0a3e3e79a3125e77cd35bed1b0d95a5
Breno Alves <breno7572@gmail.com>
2024-05-27 17:59:37 -0300

    Criando enpoint PUT

--- src/routes.js
+++ src/routes.js
@@ -50,5 +50,33 @@ export const routes = [
 
       return res.end(JSON.stringify(tasks))
     }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description } = req.body
+
+      if (!title && !description) {
+        return res.writeHead(400).end(
+          JSON.stringify({ message: 'Title or Description are required' })
+        )
+      }
+
+      const [task] = database.select('tasks', { id })
+
+      if (!task) {
+        return res.writeHead(404).end('There is no task with this id')
+      }
+
+      database.update('tasks', id, {
+        title: title ?? task.title,
+        description: description ?? task.description,
+        updated_at: new Date()
+      })
+
+      return res.writeHead(204).end('Task updated successfully!')
+    }
   }
 ]
